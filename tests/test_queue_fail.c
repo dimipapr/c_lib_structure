e2b2b4e3c57931a0a8d1aeeb197e8421f68a3fb8
Dimitris Papadimitriou <jim_pap@live.com>
2024-10-15 17:22:19 +0300

    moved

--- tests/test_queue_fail.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "queue.h"
-
-int main(){
-    puts(queue_gets());
-    fprintf(stderr,"failed test");
-    return 1;
-}
--- tests/test_queue_succ.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "queue.h"
-
-int main(){
-    puts(queue_gets());
-    return 0;
-}
