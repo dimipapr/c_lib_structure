6f5d8f7770fd6bed918857ad8760c4a447d5728a
Dimitris Papadimitriou <jim_pap@live.com>
2024-10-15 16:18:38 +0300

    added 2 basic test cases

---
+++ tests/test_queue_fail.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "queue.h"
+
+int main(){
+    puts(queue_gets());
+    fprintf(stderr,"failed test");
+    return 1;
+}
---
+++ tests/test_queue_succ.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "queue.h"
+
+int main(){
+    puts(queue_gets());
+    return 0;
+}
